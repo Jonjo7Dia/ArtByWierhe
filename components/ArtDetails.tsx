f67e7d091955d3f5f39c26cf12eb23108f8da7a9
Jonjo7Dia <jonosphgames@gmail.com>
2023-06-06 00:52:39 +0200

    Add more details page

---
+++ components/ArtDetails.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import classes from "styles/component/artWork.module.scss";
+import Image from "next/image";
+export default function ArtDetails({
+  closeModal,
+  image,
+  description,
+  title,
+  type,
+}: any) {
+  const portrait = type == "Portrait";
+  const width = 1200;
+  const height = portrait ? 1600 : 960;
+  useEffect(() => {
+    // Disable scrolling on mount
+    document.body.style.overflow = "hidden";
+
+    // Enable scrolling on unmount
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <div className={classes["artDetails"]}>
+      <div className={classes["artDetails__nav"]}>
+        <div
+          className={classes["artDetails__button"]}
+          onClick={closeModal}
+        ></div>
+      </div>
+      <h2
+        className={`${classes["artDetails__title"]} ${classes["artDetails__title--row"]}`}
+      >
+        {title}
+      </h2>
+
+      <div className={classes["artDetails__about"]}>
+        <div className={classes["artDetails__image-wrapper"]}>
+          <Image
+            src={image.url}
+            alt={image.alt}
+            width={width}
+            height={height}
+            className={`${classes["artDetails__image"]} ${
+              portrait ? classes["artDetails__image--portrait"] : ""
+            }`}
+            priority
+          />
+        </div>
+        <div className={classes["artDetails__info"]}>
+          <h2 className={classes["artDetails__title"]}>{title}</h2>
+          <p className={classes["artDetails__description"]}>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/ArtPiece.tsx
+++ components/ArtPiece.tsx
@@ -1,21 +1,57 @@
 import Image from "next/image";
+import { useState } from "react";
 import classes from "styles/component/artWork.module.scss";
+import ArtDetails from "./ArtDetails";
 export default function ArtPiece({ data }: any) {
+  const [showTitle, setShowTitle] = useState(false);
   const portrait = data.type == "Portrait";
   const image = data.image;
   const width = 1200;
   const height = portrait ? 1600 : 960;
+  const [showDetails, setShowDetails] = useState(false);
+
+  const showDetailsHandler = () => {
+    setShowDetails((prevState) => !prevState);
+  };
   return (
-    <div className={classes["artWork__piece"]}>
-      <Image
-        src={image.url}
-        alt={image.alt}
-        width={width}
-        height={height}
-        className={`${classes["artWork__piece-image"]} ${
-          portrait ? classes["artWork__piece-image--portrait"] : ""
-        }`}
-      />
-    </div>
+    <>
+      <div
+        className={classes["artWork__piece"]}
+        onMouseEnter={() => {
+          setShowTitle(true);
+        }}
+        onMouseLeave={() => {
+          setShowTitle(false);
+        }}
+        onClick={showDetailsHandler}
+      >
+        <Image
+          src={image.url}
+          alt={image.alt}
+          width={width}
+          height={height}
+          className={`${classes["artWork__piece-image"]} ${
+            portrait ? classes["artWork__piece-image--portrait"] : ""
+          }`}
+          priority
+        />
+        <div
+          className={`${classes["artWork__title"]} ${
+            showTitle ? classes["artWork__title--show"] : ""
+          }`}
+        >
+          <h2 className={classes["artWork__artName"]}>{data.art_name}</h2>
+        </div>
+      </div>
+      {showDetails && (
+        <ArtDetails
+          closeModal={showDetailsHandler}
+          image={data.image}
+          description={data.description[0].text}
+          title={data.art_name}
+          type={data.type}
+        />
+      )}
+    </>
   );
 }
